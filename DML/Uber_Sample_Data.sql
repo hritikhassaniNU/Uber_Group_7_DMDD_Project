4783fe0c22403365a307154080ca90e7c28c65f2
Hritik Hassani <hassani.h@northeastern.edu>
2025-03-24 20:50:53 -0400

    Adding sample data for vehicle

--- DML/Uber_Sample_Data.sql
+++ DML/Uber_Sample_Data.sql
@@ -273,3 +273,28 @@ INSERT INTO Driver_Status (driver_id, status_id, current_location) VALUES (7, 7,
 INSERT INTO Driver_Status (driver_id, status_id, current_location) VALUES (8, 8, '02108');  
 INSERT INTO Driver_Status (driver_id, status_id, current_location) VALUES (9, 5, '02109');  
 INSERT INTO Driver_Status (driver_id, status_id, current_location) VALUES (10, 6, '02110'); 
+
+-----------------------------------------------------------
+-- 10. Insert rows into Vehicle
+-----------------------------------------------------------
+INSERT INTO Vehicle (driver_id, ride_type_id, model, make, year, license_plate_number)
+VALUES (1, 1, 'Corolla', 'Toyota', 2020, 'PLT001');
+INSERT INTO Vehicle (driver_id, ride_type_id, model, make, year, license_plate_number)
+VALUES (2, 2, 'Civic', 'Honda', 2019, 'PLT002');
+INSERT INTO Vehicle (driver_id, ride_type_id, model, make, year, license_plate_number)
+VALUES (3, 3, 'Model S', 'Tesla', 2021, 'PLT003');
+INSERT INTO Vehicle (driver_id, ride_type_id, model, make, year, license_plate_number)
+VALUES (4, 4, 'Swift', 'Maruti', 2018, 'PLT004');
+INSERT INTO Vehicle (driver_id, ride_type_id, model, make, year, license_plate_number)
+VALUES (5, 5, 'Maruti Nexus', 'Maruti', 2020, 'PLT005');
+INSERT INTO Vehicle (driver_id, ride_type_id, model, make, year, license_plate_number)
+VALUES (6, 6, 'Camry', 'Toyota', 2017, 'PLT006');
+INSERT INTO Vehicle (driver_id, ride_type_id, model, make, year, license_plate_number)
+VALUES (7, 7, 'Verna', 'Hyundai', 2021, 'PLT007');
+INSERT INTO Vehicle (driver_id, ride_type_id, model, make, year, license_plate_number)
+VALUES (8, 1, 'Accord', 'Honda', 2019, 'PLT008');
+INSERT INTO Vehicle (driver_id, ride_type_id, model, make, year, license_plate_number)
+VALUES (9, 7, 'Fortuner', 'Toyota', 2022, 'PLT009');
+INSERT INTO Vehicle (driver_id, ride_type_id, model, make, year, license_plate_number)
+VALUES (10, 5, 'Nexon EV', 'Tata', 2023, 'PLT010');
+COMMIT;
